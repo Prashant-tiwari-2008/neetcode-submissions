4bf956ade406763dfa9f841e8b7499bed59fbaa5
Prashant-tiwari-2008 <77832133+Prashant-tiwari-2008@users.noreply.github.com>
2026-07-03 07:45:49 +0530

    Add: search-2d-matrix - submission-0

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-0.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+    searchMatrix(matrix, target) {
+     if (!matrix || matrix.length == 0 || matrix[0].length == 0) return false;
+
+    const m = matrix.length;
+    const n = matrix[0].length;
+
+    let low = 0
+    let high = (m * n) - 1
+
+    while (low <= high) {
+        let mid = Math.floor((high + low) / 2);
+
+        let row = Math.floor(mid / n);
+        let col = mid % n;
+        const midValue = matrix[row][col];
+
+        if(midValue === target){
+            return true;
+        }else if(midValue < target){
+            low = mid + 1;
+        }else{
+            high = mid -1
+        }
+    }
+
+    return false
+}
+}
