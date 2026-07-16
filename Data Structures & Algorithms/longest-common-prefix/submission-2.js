efc3d67c80df398916e32df2b3459e8d297bf684
Prashant-tiwari-2008 <77832133+Prashant-tiwari-2008@users.noreply.github.com>
2026-07-16 07:12:40 +0530

    Add: longest-common-prefix - submission-2

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-2.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string}
+     */
+    longestCommonPrefix(strs) {
+           let cp = strs[0];
+    for(let i = 1; i < strs.length; i++){
+        for(let j = 0; j < cp.length; j++){
+            if(cp[j] === strs[i][j]){
+                continue;
+            }else{
+                cp = cp.slice(0, j)
+                break;
+            }
+        }
+    }
+    return cp;
+    }
+}
