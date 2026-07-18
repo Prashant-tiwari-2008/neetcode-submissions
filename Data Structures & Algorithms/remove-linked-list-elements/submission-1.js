5ddae3aa767154d2bd8f855f324f00397f7824c7
Prashant-tiwari-2008 <77832133+Prashant-tiwari-2008@users.noreply.github.com>
2026-07-18 06:59:49 +0530

    Add: remove-linked-list-elements - submission-1

---
+++ Data Structures & Algorithms/remove-linked-list-elements/submission-1.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @param {number} val
+     * @return {ListNode}
+     */
+    removeElements(head, val) {
+        if(head === null) return null;
+        head.next = this.removeElements(head.next, val);
+        return head.val === val ? head.next : head;
+     
+    }
+}
