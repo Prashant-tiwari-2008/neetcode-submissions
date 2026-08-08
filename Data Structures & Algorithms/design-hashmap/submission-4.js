86ee73c05fced236a9e848ecc869cce8f4462903
Prashant-tiwari-2008 <77832133+Prashant-tiwari-2008@users.noreply.github.com>
2026-08-08 06:06:19 +0530

    Add: design-hashmap - submission-4

---
+++ Data Structures & Algorithms/design-hashmap/submission-4.js
@@ -0,0 +1,39 @@
+class MyHashMap {
+    constructor() {
+        this.obj = {}
+    }
+
+    /**
+     * @param {number} key
+     * @param {number} value
+     * @return {void}
+     */
+    put(key, value) {
+        this.obj[key] = value;
+    }
+
+    /**
+     * @param {number} key
+     * @return {number}
+     */
+    get(key) {
+        // return this.obj[key]  ? this.obj[key] : -1
+        return Object.keys(this.obj).includes(key.toString())  ? this.obj[key] : -1
+    }
+
+    /**
+     * @param {number} key
+     * @return {void}
+     */
+    remove(key) {
+        delete this.obj[key]
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * var obj = new MyHashMap()
+ * obj.put(key,value)
+ * var param_2 = obj.get(key)
+ * obj.remove(key)
+ */
