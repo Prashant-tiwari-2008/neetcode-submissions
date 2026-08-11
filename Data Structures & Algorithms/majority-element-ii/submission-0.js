6bd8ec19a41ac8b1de9dd1cbf18f18a95ea8d46e
Prashant-tiwari-2008 <77832133+Prashant-tiwari-2008@users.noreply.github.com>
2026-08-11 05:50:25 +0530

    Add: majority-element-ii - submission-0

---
+++ Data Structures & Algorithms/majority-element-ii/submission-0.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    majorityElement(nums) {
+        let frequency = Math.floor(nums.length/3);
+        let hashMap = {}
+        let result = new Set();
+        for(let ele of nums){
+            hashMap[ele] = (hashMap[ele] || 0) + 1;
+            if(hashMap[ele] > frequency){
+                console.log("inside if")
+                result.add(ele)
+            }
+        }
+        return [...result];
+    }
+}
