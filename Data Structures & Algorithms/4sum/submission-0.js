bd8143864d80d61f6eb5a198e8a07f2406e8e052
Prashant-tiwari-2008 <77832133+Prashant-tiwari-2008@users.noreply.github.com>
2026-09-02 08:21:22 +0530

    Add: 4sum - submission-0

---
+++ Data Structures & Algorithms/4sum/submission-0.js
@@ -0,0 +1,65 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[][]}
+     */
+    fourSum(nums, target) {
+         nums.sort((a, b) => a - b);
+
+    const result = [];
+    const n = nums.length;
+
+    for (let i = 0; i < n - 3; i++) {
+
+        // Skip duplicate first numbers
+        if (i > 0 && nums[i] === nums[i - 1]) {
+            continue;
+        }
+
+        for (let j = i + 1; j < n - 2; j++) {
+
+            // Skip duplicate second numbers
+            if (j > i + 1 && nums[j] === nums[j - 1]) {
+                continue;
+            }
+
+            let left = j + 1;
+            let right = n - 1;
+
+            while (left < right) {
+
+                const sum = nums[i] + nums[j] + nums[left] + nums[right];
+
+                if (sum === target) {
+                    result.push([
+                        nums[i],
+                        nums[j],
+                        nums[left],
+                        nums[right]
+                    ]);
+
+                    // Skip duplicates
+                    while (left < right && nums[left] === nums[left + 1]) {
+                        left++;
+                    }
+
+                    while (left < right && nums[right] === nums[right - 1]) {
+                        right--;
+                    }
+
+                    left++;
+                    right--;
+
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+    }
+
+    return result;
+    }
+}
