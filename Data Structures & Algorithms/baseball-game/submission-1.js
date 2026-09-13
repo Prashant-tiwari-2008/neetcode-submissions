9d2843e9d02093698a6598e78c9a993041e62323
Prashant-tiwari-2008 <77832133+Prashant-tiwari-2008@users.noreply.github.com>
2026-09-13 07:58:27 +0530

    Add: baseball-game - submission-1

---
+++ Data Structures & Algorithms/baseball-game/submission-1.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {string[]} operations
+     * @return {number}
+     */
+    calPoints(operations) {
+        let newStack = [];
+        let sum = 0;
+        for(let ele of operations){
+            if(ele == "C"){
+                sum -= newStack[newStack.length-1];
+                newStack.pop();
+            }else if(ele == "D"){
+                let newValue = newStack[newStack.length-1] * 2;
+                newStack.push(newValue);
+                sum += newValue;
+            }else if(ele == "+"){
+              let newValue = newStack[newStack.length -2] + newStack[newStack.length-1];
+              newStack.push(newValue);
+              sum += newValue;
+            }else{
+                newStack.push(parseInt(ele))
+                sum += parseInt(ele);
+            }
+        }
+
+        return sum;
+    }
+}
